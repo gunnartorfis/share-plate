165846e72a1d71c83732e3e00704bf69982b5aae
Gunnar Torfi <gunnartorfis@gmail.com>
2026-03-01 20:23:10 +0000

    Fix: remove broken category regex that was catching Vue data attributes

--- src/lib/server/recipes.ts
+++ src/lib/server/recipes.ts
@@ -268,20 +268,6 @@ export const fetchRecipeMetadata = createServerFn({ method: 'POST' })
           .filter(Boolean)
       }
 
-      const categoryMatch = html.match(/category["\s:=]+([^<>"']+)/i)
-      if (categoryMatch) {
-        const cats = categoryMatch[1]
-          .split(/[,;]/)
-          .map((c: string) => c.trim())
-          .filter(Boolean)
-        if (cats.length > 0) {
-          metadata.keywords = [
-            ...((metadata.keywords as string[]) || []),
-            ...cats,
-          ]
-        }
-      }
-
       const recipeJsonLd = html.match(
         /<script[^>]+type=["']application\/ld\+json["'][^>]*>([\s\S]*?)<\/script>/i,
       )
